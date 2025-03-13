eebdf78608f42cb408df1568f03f071cb8be374a
Kris Mendoza <thekittengallery@gmail.com>
2025-03-13 20:47:02 +0800

    test: ensure hit method won't exceed ship's length

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -2,7 +2,16 @@ import Ship from '../scripts/ship';
 
 const ship = new Ship(3);
 
-test('add hit count', () => {
-  ship.hit();
-  expect(ship.hitCount).toBe(1);
+describe('adding hit count to the ship', () => {
+  test('add hit count', () => {
+    ship.hit();
+    expect(ship.hitCount).toBe(1);
+  });
+
+  test('should not exceed length of ship', () => {
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.hitCount).not.toBeGreaterThan(ship.length);
+  });
 });
