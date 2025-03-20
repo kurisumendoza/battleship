3d4fd73128bb79e640925dc88d26c7b688bbb46c
Kris Mendoza <thekittengallery@gmail.com>
2025-03-20 09:48:08 +0800

    test: add failing tests for ship placement

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -12,3 +12,25 @@ test('Check if board is initialized correctly', () => {
 
   expect(isValidBoard(gameboard.board)).toBe(true);
 });
+
+describe('Verify ship placements', () => {
+  test('Check if properly placed horizontally', () => {
+    gameboard.placeShip(gameboard.ships.carrier, [9, 5], 'horizontal');
+
+    const tilesFilled = [5, 6, 7, 8, 9].every(
+      (tile) => gameboard.board[9][tile] === gameboard.ships.carrier,
+    );
+
+    expect(tilesFilled).toBe(true);
+  });
+
+  test('Check if properly placed vertically', () => {
+    gameboard.placeShip(gameboard.ships.cruiser, [0, 1], 'vertical');
+
+    const tilesFilled = [0, 1, 2].every(
+      (tile) => gameboard.board[tile][1] === gameboard.ships.cruiser,
+    );
+
+    expect(tilesFilled).toBe(true);
+  });
+});
