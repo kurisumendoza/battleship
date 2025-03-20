d9f88a93bc312e779cfc57136cfb89e34ba6d2c6
Kris Mendoza <thekittengallery@gmail.com>
2025-03-20 11:04:15 +0800

    feat: add out-of-bounds check to placeShip method

--- src/scripts/gameboard.js
+++ src/scripts/gameboard.js
@@ -10,6 +10,12 @@ class Gameboard {
   placeShip(ship, start, orientation) {
     const [row, col] = start;
 
+    if (
+      (orientation === 'vertical' && row + ship.length > this.board.length) ||
+      (orientation === 'horizontal' && col + ship.length > this.board[0].length)
+    )
+      throw new Error('Out of bounds');
+
     for (let i = 0; i < ship.length; i += 1) {
       this.board[row + (orientation === 'vertical' ? i : 0)][
         col + (orientation === 'horizontal' ? i : 0)
