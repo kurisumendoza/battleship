13413ed98b3d8cb37a38056d530fb7d89f0e0dfd
Kris Mendoza <thekittengallery@gmail.com>
2025-03-20 10:33:47 +0800

    feat: implement placeShip method to place ships on board

--- src/scripts/gameboard.js
+++ src/scripts/gameboard.js
@@ -5,6 +5,17 @@ class Gameboard {
     this.board = new Array(10).fill(null).map(() => new Array(10).fill(null));
     this.ships = initializeShips();
   }
+
+  // Places specified ships on the board
+  placeShip(ship, start, orientation) {
+    const [row, col] = start;
+
+    for (let i = 0; i < ship.length; i += 1) {
+      this.board[row + (orientation === 'vertical' ? i : 0)][
+        col + (orientation === 'horizontal' ? i : 0)
+      ] = ship;
+    }
+  }
 }
 
 export default Gameboard;
