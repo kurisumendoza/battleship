e0a56e2e8286f065e55635dbae3a55c312c77600
Kris Mendoza <thekittengallery@gmail.com>
2025-03-25 14:03:41 +0800

    feat: add selectors.js with initial query selectors

---
+++ src/scripts/selectors.js
@@ -0,0 +1,16 @@
+const header = document.querySelector('.header');
+
+const gameboardUI = {
+  board: document.querySelector('.gameboard'),
+};
+
+const controlsUI = {
+  controls: document.querySelector('.controls'),
+};
+
+const playerStatsUI = {
+  p1Stats: document.querySelector('.player1-stats'),
+  p2Stats: document.querySelector('.player2-stats'),
+};
+
+export { header, gameboardUI, controlsUI, playerStatsUI };
