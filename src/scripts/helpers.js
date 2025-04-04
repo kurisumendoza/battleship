870f9edffdb3123a4aff82d1b9cf4e9fb19cf281
Kris Mendoza <thekittengallery@gmail.com>
2025-04-04 17:05:08 +0800

    feat: add createElement helper function

--- src/scripts/helpers.js
+++ src/scripts/helpers.js
@@ -1,7 +1,16 @@
-// Shows or hides an element
+// Helper function for showing and hiding an element
 const toggleElement = (el, display) => {
   const element = el;
   element.style.display = display;
 };
 
-export default toggleElement;
+// Helper function for creating elements
+const createElement = (type, classList = []) => {
+  const element = document.createElement(type);
+
+  if (classList.length !== 0) element.classList.add(...classList);
+
+  return element;
+};
+
+export { toggleElement, createElement };
