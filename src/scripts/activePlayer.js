5705f57a9c6ba41e8b492b3b08a536818c4431d4
Kris Mendoza <thekittengallery@gmail.com>
2025-04-17 11:11:43 +0800

    feat: add opponent tracking to activePlayer

--- src/scripts/activePlayer.js
+++ src/scripts/activePlayer.js
@@ -4,14 +4,18 @@ import { players } from './createPlayers';
 const activePlayer = {
   player: null,
   gameboard: null,
+  opponent: null,
+  oppGameboard: null,
   assign() {
     this.player = players.player1;
     this.gameboard = this.player.gameboard;
+    this.opponent = players.player2;
+    this.oppGameboard = this.opponent.gameboard;
   },
   switch() {
-    this.player =
-      this.player === players.player1 ? players.player2 : players.player1;
+    [this.player, this.opponent] = [this.opponent, this.player];
     this.gameboard = this.player.gameboard;
+    this.oppGameboard = this.opponent.gameboard;
   },
 };
 
