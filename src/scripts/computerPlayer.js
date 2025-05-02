f80f5dc7ff718b7f55d6406e1c3161126180fadc
Kris Mendoza <thekittengallery@gmail.com>
2025-05-02 17:40:15 +0800

    wip: make autoPlaceShips() test pass

--- src/scripts/computerPlayer.js
+++ src/scripts/computerPlayer.js
@@ -6,7 +6,37 @@ class ComputerPlayer extends Player {
     this.name = 'Computer';
   }
 
-  autoPlaceShips() {}
+  // Automatically place all ships in the computer player's gameboard
+  autoPlaceShips() {
+    Object.values(this.gameboard.ships).forEach((ship) => {
+      const orientation = Math.random() < 0.5 ? 'horizontal' : 'vertical';
+      this.gameboard.placeShip(
+        ship,
+        this.#autoPlacementCoord(ship.length, orientation),
+        orientation,
+      );
+    });
+  }
+
+  // Creates random coordinates to be used for placing ships
+  #autoPlacementCoord(length, orientation) {
+    const rowSize = 10 - (orientation === 'vertical' ? length : 0);
+    const colSize = 10 - (orientation === 'horizontal' ? length : 0);
+
+    const row = Math.floor(Math.random() * rowSize);
+    const col = Math.floor(Math.random() * colSize);
+
+    for (let i = 0; i < length; i += 1) {
+      if (
+        this.gameboard.occupied.has(
+          `${row + (orientation === 'vertical' ? i : 0)}, ${col + (orientation === 'horizontal' ? i : 0)}`,
+        )
+      )
+        return this.#autoPlacementCoord(length, orientation);
+    }
+
+    return [row, col];
+  }
 }
 
 export default ComputerPlayer;
