f69c34eb0c04173724c9d77bd9d7d4962522a4a4
Kris Mendoza <thekittengallery@gmail.com>
2025-05-02 20:06:38 +0800

    test: add test to verify ship placement validity
    Test may pass or fail due to randomness.
    The autoPlaceShips method should ensure ships are placed at least one cell apart.

--- src/tests/computerPlayer.test.js
+++ src/tests/computerPlayer.test.js
@@ -12,4 +12,42 @@ describe('Automatic Ship Placement', () => {
 
     expect(computer.gameboard.isAllPlaced()).toBe(true);
   });
+
+  test('Ensure that each ship is at least one cell apart', () => {
+    computer.autoPlaceShips();
+
+    const offsets = [
+      [-1, 0],
+      [1, 0],
+      [0, -1],
+      [0, 1],
+      [-1, -1],
+      [-1, 1],
+      [1, -1],
+      [1, 1],
+    ];
+
+    const validPlacement = Object.values(computer.gameboard.ships).every(
+      (ship) => {
+        return ship.position.every(([x, y]) => {
+          for (let i = 0; i < offsets.length; i += 1) {
+            const newX = offsets[i][0] + x;
+            const newY = offsets[i][1] + y;
+
+            if (newX >= 0 && newX < 10 && newY >= 0 && newY < 10) {
+              const isNull = computer.gameboard.board[newX][newY] === null;
+              const isPartOfShip = ship.position.some(
+                ([posX, posY]) => posX === newX && posY === newY,
+              );
+
+              if (!isNull && !isPartOfShip) return false;
+            }
+          }
+          return true;
+        });
+      },
+    );
+
+    expect(validPlacement).toBe(true);
+  });
 });
