2c2c8f18806d2cca16aec475962f79b9622ef52f
Kris Mendoza <thekittengallery@gmail.com>
2025-05-26 20:15:08 +0800

    fix: reset player status UI when replaying

--- src/scripts/resetGame.js
+++ src/scripts/resetGame.js
@@ -5,13 +5,30 @@ import { togglePlayAgainBtn } from './gameplayUI';
 import initializeGame from './gameSetupController';
 import { rerenderStartScreen } from './gameSetupUI';
 import { resetGameStatusUI } from './gameStatusUI';
-import { controlsUI, gameboardUI } from './selectors';
+import { controlsUI, gameboardUI, playerStatsUI } from './selectors';
+
+const resetRemainingShipsUI = () => {
+  const shipEl = document.querySelectorAll('.remaining-ships-container div');
+
+  shipEl.forEach((ship) => {
+    const shipDiv = ship;
+
+    shipDiv.textContent = '';
+    shipDiv.style.filter = '';
+  });
+};
 
 const resetGame = () => {
   gameState.hasAttacked = false;
   gameState.hasWinner = false;
   resetPlayers();
   activePlayer.reset();
+
+  playerStatsUI.p1Health.style.width = '100%';
+  playerStatsUI.p2Health.style.width = '100%';
+  playerStatsUI.p1Health.style.backgroundColor = '';
+  playerStatsUI.p2Health.style.backgroundColor = '';
+  resetRemainingShipsUI();
 
   gameboardUI.yourBoard.innerHTML = '';
   gameboardUI.board.innerHTML = '';
